cb4d75424844a90862da072c215bc73e60dcb3c2
unknown <aavik743@gmail.com>
2022-02-10 15:40:07 +0530

    added: push(item) method

---
+++ StaQue.java
@@ -0,0 +1,20 @@
+package stque;
+
+import java.util.LinkedList;
+
+public class StaQue {
+	
+	public static void main(String args[]){
+
+		 LinkedList list=new LinkedList();
+
+	     //Adding elements to the Linked list
+		 list.push(70);
+		 list.push(30);
+		 list.push(56);
+	  
+	     System.out.println(list);
+	   
+	   } 
+
+}
